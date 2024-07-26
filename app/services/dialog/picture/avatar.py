b2eb3e87023ccc8b370604b3f96d0b55a6b70226
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-07-26 22:30:22 +0200

    Update avatar.py

--- app/services/dialog/picture/avatar.py
+++ app/services/dialog/picture/avatar.py
@@ -3,13 +3,13 @@ from PIL import Image
 from services.lib.draw_utils import image_square_crop
 from services.lib.utils import async_wrap
 
-THOR_AVA_FRAME_PATH = './data/thor_ava_frame.png'
-THOR_LASER_PATH = './data/laser_green_2.png'
-THOR_LASER_SIZE = 24
+MAYA_AVA_FRAME_PATH = './data/maya_ava_frame.png'
+MAYA_LASER_PATH = './data/laser_green_2.png'
+MAYA_LASER_SIZE = 24
 
 
 def combine_frame_and_photo(photo: Image.Image):
-    frame = Image.open(THOR_AVA_FRAME_PATH)
+    frame = Image.open(MAYA_AVA_FRAME_PATH)
 
     photo = photo.resize(frame.size).convert('RGBA')
     result = Image.alpha_composite(photo, frame)
